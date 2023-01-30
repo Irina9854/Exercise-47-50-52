2d9b9a0060a3c193611f088d8b607508ff8fa65d
Irina Gorynina <iren.gorynina@gmail.com>
2023-01-30 13:26:51 +0300

    Решение 47 задачи

---
+++ Exercise47/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+int rows = ReadInt("Введите число m: ");
+int colums = ReadInt("Введите число n: ");
+double[,] numbers = new double[rows, colums];
+FillArrayWithRandom(numbers);
+PrintArray(numbers);
+
+void FillArrayWithRandom(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
